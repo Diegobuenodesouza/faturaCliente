a9d906d862a7a67bbe1bfc3060daf138d4406fd6
Diego Souza <diegobueno.souza@gmail.com>
2021-01-10 20:52:23 -0300

    Finalizado adicionar servicos a diversos clientes

--- src/app/home/utilitarios/add-servicos-diversos-clientes/adicionar-servicos/adicionar-servicos.component.ts
+++ src/app/home/utilitarios/add-servicos-diversos-clientes/adicionar-servicos/adicionar-servicos.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ConsultaClientesService } from 'src/app/consulta-clientes.service';
+import { Cliente } from 'src/app/_model/cliente';
+import { Servico } from 'src/app/_model/servico';
 
 @Component({
   selector: 'app-adicionar-servicos',
@@ -9,20 +14,53 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AdicionarServicosComponent implements OnInit {
 
   @Input() listaCliente = new FormArray([])
-
   listadeServicos = new FormArray([])
   
-  constructor() { }
+  constructor(
+    private consutalCliente: ConsultaClientesService,
+    private toastr : ToastrService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void { 
   }
 
   adicionarServico(): void{
-    this.listadeServicos.push(new FormGroup({
+    let formServico = new FormGroup({
       descricao : new FormControl('', [Validators.required]),
       vencimento: new FormControl('', [Validators.required]),
-      valor: new FormControl('', [Validators.required, Validators.min(-1)])
-    }))
+      valor: new FormControl('', [Validators.required, Validators.min(1)])
+    })
+    this.listadeServicos.push(formServico)
   }
 
+  removeServico(index: number) : void {
+    this.listadeServicos.removeAt(index)
+  }
+
+  setServicos(): void {
+    this.listaCliente.controls.forEach((cliente: FormGroup) => {
+      this.consutalCliente.getIdCliente(cliente.value.cnpj).subscribe(
+        (resposta) => {
+          let chave = Object.keys(resposta)[0];
+          let cliente: Cliente = Object.values(resposta)[0];
+          this.listadeServicos.controls.forEach((formGroup: FormGroup) => {
+            let servico : Servico = formGroup.value
+            if ( cliente.listaServico === undefined) {              
+              cliente.listaServico =  [];
+              cliente.listaServico.push(servico)
+            }
+            else{              
+              cliente.listaServico.push(servico)
+            }            
+          });
+          this.consutalCliente.putCliente(chave, cliente).subscribe(
+            () => this.router.navigate(['/home'])
+          );         
+        }
+        );
+      }    
+      )     
+      this.toastr.success('Serviços adicionados  com sucesso')
+  }
 }
--- src/app/home/utilitarios/add-servicos-diversos-clientes/adicionar-servicos/dist/adicionar-servicos.component.js
+++ src/app/home/utilitarios/add-servicos-diversos-clientes/adicionar-servicos/dist/adicionar-servicos.component.js
@@ -10,18 +10,46 @@ exports.AdicionarServicosComponent = void 0;
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
 var AdicionarServicosComponent = /** @class */ (function () {
-    function AdicionarServicosComponent() {
+    function AdicionarServicosComponent(consutalCliente, toastr, router) {
+        this.consutalCliente = consutalCliente;
+        this.toastr = toastr;
+        this.router = router;
         this.listaCliente = new forms_1.FormArray([]);
         this.listadeServicos = new forms_1.FormArray([]);
     }
     AdicionarServicosComponent.prototype.ngOnInit = function () {
     };
     AdicionarServicosComponent.prototype.adicionarServico = function () {
-        this.listadeServicos.push(new forms_1.FormGroup({
+        var formServico = new forms_1.FormGroup({
             descricao: new forms_1.FormControl('', [forms_1.Validators.required]),
             vencimento: new forms_1.FormControl('', [forms_1.Validators.required]),
-            valor: new forms_1.FormControl('', [forms_1.Validators.required, forms_1.Validators.min(-1)])
-        }));
+            valor: new forms_1.FormControl('', [forms_1.Validators.required, forms_1.Validators.min(1)])
+        });
+        this.listadeServicos.push(formServico);
+    };
+    AdicionarServicosComponent.prototype.removeServico = function (index) {
+        this.listadeServicos.removeAt(index);
+    };
+    AdicionarServicosComponent.prototype.setServicos = function () {
+        var _this = this;
+        this.listaCliente.controls.forEach(function (cliente) {
+            _this.consutalCliente.getIdCliente(cliente.value.cnpj).subscribe(function (resposta) {
+                var chave = Object.keys(resposta)[0];
+                var cliente = Object.values(resposta)[0];
+                _this.listadeServicos.controls.forEach(function (formGroup) {
+                    var servico = formGroup.value;
+                    if (cliente.listaServico === undefined) {
+                        cliente.listaServico = [];
+                        cliente.listaServico.push(servico);
+                    }
+                    else {
+                        cliente.listaServico.push(servico);
+                    }
+                });
+                _this.consutalCliente.putCliente(chave, cliente).subscribe(function () { return _this.router.navigate(['/home']); });
+            });
+        });
+        this.toastr.success('Serviços adicionados  com sucesso');
     };
     __decorate([
         core_1.Input()
